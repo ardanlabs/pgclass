24a7cdb0e686987376b54082877843149376263c
William Kennedy <bill@ardanlabs.com>
2024-05-17 13:36:08 -0400

    commit 41

---
+++ business/domain/userbus/userbus_test.go
@@ -0,0 +1,142 @@
+package userbus_test
+
+import (
+	"context"
+	"fmt"
+	"runtime/debug"
+	"sort"
+	"testing"
+	"time"
+
+	"github.com/ardanlabs/service/business/domain/userbus"
+	"github.com/ardanlabs/service/business/sdk/dbtest"
+	"github.com/ardanlabs/service/business/sdk/unitest"
+	"github.com/google/go-cmp/cmp"
+)
+
+func Test_User(t *testing.T) {
+	t.Parallel()
+
+	db := dbtest.NewDatabase(t, "Test_User")
+	defer func() {
+		if r := recover(); r != nil {
+			t.Log(r)
+			t.Error(string(debug.Stack()))
+		}
+		db.Teardown()
+	}()
+
+	sd, err := insertSeedData(db.BusDomain)
+	if err != nil {
+		t.Fatalf("Seeding error: %s", err)
+	}
+
+	// -------------------------------------------------------------------------
+
+	unitest.Run(t, query(db.BusDomain, sd), "query")
+	// unitest.Run(t, create(db.BusDomain), "create")
+	// unitest.Run(t, update(db.BusDomain, sd), "update")
+	// unitest.Run(t, delete(db.BusDomain, sd), "delete")
+}
+
+// =============================================================================
+
+func insertSeedData(busDomain dbtest.BusDomain) (unitest.SeedData, error) {
+	ctx := context.Background()
+
+	usrs, err := userbus.TestSeedUsers(ctx, 2, userbus.Roles.Admin, busDomain.User)
+	if err != nil {
+		return unitest.SeedData{}, fmt.Errorf("seeding users : %w", err)
+	}
+
+	tu1 := unitest.User{
+		User: usrs[0],
+	}
+
+	tu2 := unitest.User{
+		User: usrs[1],
+	}
+
+	// -------------------------------------------------------------------------
+
+	usrs, err = userbus.TestSeedUsers(ctx, 2, userbus.Roles.User, busDomain.User)
+	if err != nil {
+		return unitest.SeedData{}, fmt.Errorf("seeding users : %w", err)
+	}
+
+	tu3 := unitest.User{
+		User: usrs[0],
+	}
+
+	tu4 := unitest.User{
+		User: usrs[1],
+	}
+
+	// -------------------------------------------------------------------------
+
+	sd := unitest.SeedData{
+		Users:  []unitest.User{tu3, tu4},
+		Admins: []unitest.User{tu1, tu2},
+	}
+
+	return sd, nil
+}
+
+// =============================================================================
+
+func query(busDomain dbtest.BusDomain, sd unitest.SeedData) []unitest.Table {
+	usrs := make([]userbus.User, 0, len(sd.Admins)+len(sd.Users))
+
+	for _, adm := range sd.Admins {
+		usrs = append(usrs, adm.User)
+	}
+
+	for _, usr := range sd.Users {
+		usrs = append(usrs, usr.User)
+	}
+
+	sort.Slice(usrs, func(i, j int) bool {
+		return usrs[i].ID.String() <= usrs[j].ID.String()
+	})
+
+	table := []unitest.Table{
+		{
+			Name:    "all",
+			ExpResp: usrs,
+			ExcFunc: func(ctx context.Context) any {
+				filter := userbus.QueryFilter{
+					Name: dbtest.UserNamePointer(userbus.Names.MustParse("Name")),
+				}
+
+				resp, err := busDomain.User.Query(ctx, filter, userbus.DefaultOrderBy, 1, 10)
+				if err != nil {
+					return err
+				}
+
+				return resp
+			},
+			CmpFunc: func(got any, exp any) string {
+				gotResp, exists := got.([]userbus.User)
+				if !exists {
+					return "error occurred"
+				}
+
+				expResp := exp.([]userbus.User)
+
+				for i := range gotResp {
+					if gotResp[i].DateCreated.Format(time.RFC3339) == expResp[i].DateCreated.Format(time.RFC3339) {
+						expResp[i].DateCreated = gotResp[i].DateCreated
+					}
+
+					if gotResp[i].DateUpdated.Format(time.RFC3339) == expResp[i].DateUpdated.Format(time.RFC3339) {
+						expResp[i].DateUpdated = gotResp[i].DateUpdated
+					}
+				}
+
+				return cmp.Diff(gotResp, expResp)
+			},
+		},
+	}
+
+	return table
+}
